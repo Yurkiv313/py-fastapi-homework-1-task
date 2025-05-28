062cecd0dbd16c616b77ccd3009bc87ee511f64d
Andrii <yurkivandriy02@gmail.com>
2025-05-28 19:17:39 +0300

    Completed task Online Cinema Part 1 fix

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -19,6 +19,9 @@ async def read_movies(
 
     total_pages = (total_items + per_page - 1) // per_page
 
+    if total_items == 0 or page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
     base_url = "/api/v1/theater/movies"
     prev_page = f"{base_url}/?page={page - 1}&per_page={per_page}" if page > 1 else None
     next_page = f"{base_url}/?page={page + 1}&per_page={per_page}" if page < total_pages else None
@@ -26,8 +29,6 @@ async def read_movies(
     result = await db.execute(select(MovieModel).offset(offset).limit(per_page))
     movies = result.scalars().all()
 
-    if not movies:
-        raise HTTPException(status_code=404, detail="No movies found.")
 
     return MovieListResponseSchema(
         movies=movies,
